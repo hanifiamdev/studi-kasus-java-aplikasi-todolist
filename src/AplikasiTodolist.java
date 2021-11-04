17ee56d826453aa2b81fa934a9b1c3b227839524
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-04 10:53:22 +0700

    Menghapus Todolist

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -71,8 +71,15 @@ public class AplikasiTodolist {
     /**
      * Menghapus todo dari list
      */
-    public static void removeTodoList() {
-
+    public static boolean removeTodoList(Integer number) {
+        if ((number - 1) >= model.length) {
+            return false;
+        }else if(model[number-1] == null) {
+            return false;
+        }else {
+            model[number - 1] = null;
+            return true;
+        }
     }
 
     /**
