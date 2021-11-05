20061f3bdc01756b9ef3a04918d5b543ce304658
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-06 03:41:13 +0700

    View Menghapus Todolist

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -186,6 +186,16 @@ public class AplikasiTodolist {
      * Menampilkan view menghapus todo list
      */
     public static void viewRemoveTodoList() {
+        System.out.println("MENGHAPUS TODOLIST");
 
+        var number = input("Nomor yang Dihapus (x Jika Batal)");
+        if(number.equals("x")) {
+            // batal
+        } else {
+            boolean success = removeTodoList(Integer.valueOf(number));
+            if (!success) {
+                System.out.println("Gagal menghapus todolist : " + number);
+            }
+        }
     }
 }
