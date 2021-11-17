95e32998bd7a2dfa490b3064404ae3d3e4ea4948
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-18 04:55:05 +0700

    Membuat entity dan repository

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -5,7 +5,7 @@ public class AplikasiTodolist {
     public static java.util.Scanner scanner = new java.util.Scanner(System.in);
 
     public static void main(String[] args) {
-        testviewAddTodoList();
+        viewShowTodoList();
     }
 
     /**
@@ -198,4 +198,18 @@ public class AplikasiTodolist {
             }
         }
     }
+
+    public static void testViewRemoveTodoList() {
+        addTodoList("Satu");
+        addTodoList("Dua");
+        addTodoList("Tiga");
+
+        showTodoList();
+
+        viewRemoveTodoList();
+
+        showTodoList();
+
+    }
+
 }
