7b36017c959ebdac0fabf76b5102f558a4705e20
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-18 05:51:02 +0700

    Test Repository & Service : Menambah Todolist

--- src/test/service/TodoListServiceTest.java
+++ src/test/service/TodoListServiceTest.java
@@ -8,7 +8,7 @@ import service.TodoListServiceImpl;
 
 public class TodoListServiceTest {
     public static void main(String[] args) {
-        testShowTodoList();
+        testAddTodoList();
     }
 
     public static void testShowTodoList() {
@@ -21,5 +21,16 @@ public class TodoListServiceTest {
         TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
 
         todoListService.showTodoList();
+    }
+
+    public static void testAddTodoList() {
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        todoListService.addTodoList("Belajar Java Dasar");
+        todoListService.addTodoList("Belajar Java OOP");
+        todoListService.addTodoList("Belajar Java Standard Classses");
+
+        todoListService.showTodoList();
+
     }
 }
