d30cbba8cc925a8c1c548da71dbd30fa52760511
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-18 05:57:07 +0700

    Repository & Service : Menghapus Todolist

--- src/service/TodoListServiceImpl.java
+++ src/service/TodoListServiceImpl.java
@@ -38,6 +38,13 @@ public class TodoListServiceImpl implements TodoListService {
 
     @Override
     public void removeTodoList(Integer number) {
+        boolean success = todoListRepository.remove(number);
+
+        if(success) {
+            System.out.println("SUKSES MENGHAPUS TODO : " + number);
+        } else {
+            System.out.println("GAGAL MENGHAPUS TODO : " + number);
+        }
 
     }
 }
