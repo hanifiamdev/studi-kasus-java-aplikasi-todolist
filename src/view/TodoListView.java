a3f17ae26b809aa315a1bfa840632d6b5c29c181
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-18 06:36:53 +0700

    View : Menghapus Todolist

--- src/view/TodoListView.java
+++ src/view/TodoListView.java
@@ -47,6 +47,13 @@ public class TodoListView {
     }
 
     public void removeTodoList(){
+        System.out.println("MENGHAPUS TODOLIST");
 
+        var number = InputUtil.input("Nomor yang Dihapus (x Jika Batal)");
+        if(number.equals("x")) {
+            // batal
+        } else {
+            todoListService.removeTodoList(Integer.valueOf(number));
+        }
     }
 }
